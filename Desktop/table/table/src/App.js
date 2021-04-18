5b4a37a32589619537210cde84fad61245232448
jojo-joseph <jojojoseph1996@gmail.com>
2021-04-18 22:07:46 +0530

    initial submit

---
+++ Desktop/table/table/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StickyHeadTable from './table';
+function App() {
+  return (
+    <div className="App">
+     <StickyHeadTable></StickyHeadTable>
+     
+    </div>
+  );
+}
+
+export default App;
